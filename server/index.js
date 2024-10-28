46872d1c4502050e9102c83e9fc990a786e6d9fc
kumar7505 <kumarpers7@gmail.com>
2024-10-28 18:50:10 +0530

    feat: setting up the server connectivity

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 PORT = process.env.PORT;
+Mongo_PORT = process.env.Mongo_PORT;
 
 const app = express();
 app.use(cors())
 app.use(express.json());
 
+mongoose.connect(`${Mongo_PORT}/test`);
+
+app.post('/add', (req, res, next) => {
+    const task = req.body.task;
+})
 
 app.listen(PORT, () => {
     console.log("Server is Running");
--- todo frontend/src/components/Create.jsx
+++ todo frontend/src/components/Create.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import './Create.css'
+import React from "react";
+import "./Create.css";
 
 const Create = () => {
+  const PORT = process.env.PORT;
+  const [task, setTask] = useState();
+  const handleAdd = () => {
+    axios.post(`http://localhost:${PORT}/add`, {task: task})
+      .then(result => console.log(result))
+      .catch(err => console.log(error));
+  };
   return (
-    <div className='create_form'>
-        <input type="text" name="" id="" />
-        <button type="button">Add</button>
+    <div className="create_form">
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={(e) => {
+          setTask(e.target.value);
+        }}
+      />
+      <button type="button">Add</button>
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
